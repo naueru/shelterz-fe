00d057edda08aedd5df78a3921fb9e90ffa7e1a6
Nahuel Andrada <nahuel@euroasiapay.com>
2025-06-10 23:23:34 -0300

    Creates dynamic field component.

---
+++ src/components/ui/field/Field.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  name: string
+  modelValue: string
+  type?: HTMLInputElement['type']
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+function onInput(event: Event) {
+  const target = event.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <div class="container">
+    <label :for="`field_${name}`">{{ label }}</label>
+    <input :type="type" :id="`field_${name}`" :name="name" :value="modelValue" @input="onInput" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
