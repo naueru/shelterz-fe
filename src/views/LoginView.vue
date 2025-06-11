e07332938fd0a2af84a0ff354d0faac026fe68ae
Nahuel Andrada <nahuel@euroasiapay.com>
2025-06-10 23:43:57 -0300

    Creates Login View

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// Router
+import { useRouter } from 'vue-router'
+
+// Store
+import store from '@/store/store'
+
+// Component
+import LoginForm from '@/components/features/login/LoginForm.vue'
+
+const router = useRouter()
+
+const login = store((state) => state.login)
+const token = store((state) => state.token)
+
+function handleSubmit(data: Record<string, string>) {
+  login(data.email, data.password, router)
+}
+</script>
+
+<template>{{ token }}<LoginForm :onSubmit="handleSubmit" /></template>
