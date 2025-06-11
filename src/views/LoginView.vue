4f0ea22125ac5ea639a45cf053773f2e287498a6
Nahuel Andrada <nahuel@euroasiapay.com>
2025-06-10 23:44:47 -0300

    Removes token from Login View

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,11 +11,10 @@ import LoginForm from '@/components/features/login/LoginForm.vue'
 const router = useRouter()
 
 const login = store((state) => state.login)
-const token = store((state) => state.token)
 
 function handleSubmit(data: Record<string, string>) {
   login(data.email, data.password, router)
 }
 </script>
 
-<template>{{ token }}<LoginForm :onSubmit="handleSubmit" /></template>
+<template><LoginForm :onSubmit="handleSubmit" /></template>
