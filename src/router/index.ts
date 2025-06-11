59aa6cc17ff9c1518c346ceb21be43e595b1779f
Nahuel Andrada <nahuel@euroasiapay.com>
2025-06-10 23:53:16 -0300

    Updates routes and implements nested routes. Implements new layouts components. Adds ward for authentication.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,63 +1,96 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DashboardView from '../views/DashboardView.vue'
+
+// Store
+import store from '@/store/store'
+
+// Components
+import MainLayout from '@/components/layout/MainLayout/MainLayout.vue'
+import AuthLayout from '@/components/layout/AuthLayout/AuthLayout.vue'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: DashboardView,
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'dashboard',
+          component: DashboardView,
+        },
+        {
+          path: '/build',
+          name: 'build',
+          component: () => import('@/views/BuildView.vue'),
+        },
+        {
+          path: '/research',
+          name: 'research',
+          component: () => import('@/views/ResearchView.vue'),
+        },
+        {
+          path: '/resources',
+          name: 'resources',
+          component: () => import('@/views/ResourcesView.vue'),
+        },
+        {
+          path: '/assignments',
+          name: 'assignments',
+          component: () => import('@/views/AssignmentsView.vue'),
+        },
+        {
+          path: '/map',
+          name: 'map',
+          component: () => import('@/views/MapView.vue'),
+        },
+        {
+          path: '/military',
+          name: 'military',
+          component: () => import('@/views/MilitaryView.vue'),
+        },
+        {
+          path: '/diplomacy',
+          name: 'diplomacy',
+          component: () => import('@/views/DiplomacyView.vue'),
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('@/views/AboutView.vue'),
+        },
+      ],
     },
     {
-      path: '/build',
-      name: 'build',
-      component: () => import('../views/BuildView.vue'),
-    },
-    {
-      path: '/research',
-      name: 'research',
-      component: () => import('../views/ResearchView.vue'),
-    },
-    {
-      path: '/resources',
-      name: 'resources',
-      component: () => import('../views/ResourcesView.vue'),
-    },
-    {
-      path: '/assignments',
-      name: 'assignments',
-      component: () => import('../views/AssignmentsView.vue'),
-    },
-    {
-      path: '/map',
-      name: 'map',
-      component: () => import('../views/MapView.vue'),
-    },
-    {
-      path: '/military',
-      name: 'military',
-      component: () => import('../views/MilitaryView.vue'),
-    },
-    {
-      path: '/diplomacy',
-      name: 'diplomacy',
-      component: () => import('../views/DiplomacyView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'notFound',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/NotFoundView.vue'),
+      path: '/auth',
+      component: AuthLayout,
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/views/LoginView.vue'),
+        },
+        {
+          path: 'signup',
+          name: 'signup',
+          component: () => import('@/views/SignupView.vue'),
+        },
+      ],
     },
   ],
+})
+
+router.beforeEach((to, _from, next) => {
+  console.log('before Each')
+
+  const isLoggedIn = store((state) => state.isLoggedIn)
+
+  if (to.name !== 'login' && to.name !== 'signup' && !isLoggedIn) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
