9d50192cc4976168b68d23f9bb1c2a827338df6f
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-06 17:31:40 -0300

    Creates Headline component

---
+++ src/components/ui/headline/Headline.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{}>()
+</script>
+
+<template>
+  <h3 class="headlineContainer"><slot></slot></h3>
+</template>
+
+<style scoped>
+.headlineContainer {
+  display: flex;
+  font-size: 20px;
+}
+</style>
