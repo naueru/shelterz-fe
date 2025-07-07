2260eafbb6052e007ff61d79fb498b99c9f4c68e
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-06 22:40:02 -0300

    Adds link to signup

--- src/components/features/login/LoginForm.vue
+++ src/components/features/login/LoginForm.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+// Router
+import { useRouter } from 'vue-router'
+
 // Components
 import Form from '@/components/ui/form/Form.vue'
 
 // Types
 import type { IObject } from '@/types/generics'
+
+const router = useRouter()
+
+const goToSignup = () => {
+  router.push({ name: 'signup' })
+}
 
 const props = defineProps<{
   onSubmit: (data: IObject) => void
@@ -21,7 +30,7 @@ const formData = ref({
   password: '',
 })
 
-function handleSubmit(data: Record<string, string>) {
+const handleSubmit = (data: IObject) => {
   props.onSubmit(data)
 }
 </script>
@@ -33,5 +42,7 @@ function handleSubmit(data: Record<string, string>) {
     title="Login"
     @submit="handleSubmit"
     submit-label="Login"
+    cancel-label="or Signup"
+    :handle-cancel="goToSignup"
   ></Form>
 </template>
