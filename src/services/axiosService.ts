fd5023b7c2a6f2addb968b44cbf62917978fcb24
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-28 00:16:13 -0300

    Adds signup logic

--- src/services/axiosService.ts
+++ src/services/axiosService.ts
@@ -1,13 +1,13 @@
 import httpClient from '@/services/httpClient'
 
 const axiosService = () => {
-  const getUser = (id: string) => httpClient.get(`/users/${id}`)
+  const getUser = (id: string) => httpClient.get(`/api/users/${id}`)
 
   const login = (email: string, password: string) =>
     httpClient.post(`/api/users/login`, { email, password })
 
-  const createUser = (data: { username: string; password: string }) =>
-    httpClient.post('/users', data)
+  const createUser = (data: { userName: string; email: string; password: string; image: string }) =>
+    httpClient.post('/api/users/signup', data)
 
   return {
     getUser,
