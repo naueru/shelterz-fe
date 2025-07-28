60f38ef58572a8d210b7ef0ace7515a2e03d9b6b
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-28 00:14:28 -0300

    Adds ISelectOption

--- src/types/generics.ts
+++ src/types/generics.ts
@@ -3,3 +3,8 @@ export type TObject = Record<string, string>
 export interface IObject<T = any> {
   [key: string]: T
 }
+
+export interface ISelectOption {
+  label: string
+  value: string | number
+}
