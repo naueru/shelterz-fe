429ea873cd86c884a97965010fe418ecefc2ddd2
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-30 23:03:08 -0300

    Addss styles to center contents.

--- src/views/MilitaryView.vue
+++ src/views/MilitaryView.vue
@@ -1,3 +1,16 @@
 <script setup lang="ts"></script>
 
-<template>Military</template>
+<template><section class="militaryViewContainer">Military</section></template>
+
+<style scoped>
+.militaryViewContainer {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  width: 100%;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
