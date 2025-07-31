822b8dbd7d57173edbbadb996304126295e0a627
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-30 23:02:44 -0300

    Addss styles to center contents.

--- src/views/DiplomacyView.vue
+++ src/views/DiplomacyView.vue
@@ -1,3 +1,16 @@
 <script setup lang="ts"></script>
 
-<template>Diplomacy</template>
+<template><section class="diplomacyViewContainer">Diplomacy</section></template>
+
+<style scoped>
+.diplomacyViewContainer {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  width: 100%;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
