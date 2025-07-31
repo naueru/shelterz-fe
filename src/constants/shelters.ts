e8d2d48560afbf4c2d9fa8803bc20342124ebeba
Nahuel Andrada <nahuel@euroasiapay.com>
2025-07-30 22:33:34 -0300

    Adds RESOURCES lost constant

---
+++ src/constants/shelters.ts
@@ -0,0 +1,8 @@
+export const RESOURCES: { amount: number | string; label: string; name: string; icon: string }[] = [
+  { name: 'wood', label: 'Wood', amount: 0, icon: 'wood' },
+  { name: 'food', label: 'food', amount: 0, icon: 'food' },
+  { name: 'water', label: 'water', amount: 0, icon: 'water' },
+  { name: 'medicalSupplies', label: 'Meds', amount: 0, icon: 'medicalSupplies' },
+  { name: 'fuel', label: 'fuel', amount: 0, icon: 'fuel' },
+  { name: 'population', label: 'population', amount: 0, icon: 'population' },
+]
